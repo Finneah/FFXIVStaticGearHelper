7ff88c9ae0e80898a5b85388f78d49cffe4f8ecb
Finneah <jennifer.schnaible86@gmail.com>
2022-08-07 13:14:16 +0200

    refactor(base): remove GUILD_ID from env

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,3 @@ dotenv.config();
 
 export const TOKEN = process.env.TOKEN || '';
 export const APP_ID = process.env.APP_ID || '';
-export const GUILD_ID = process.env.GUILD_ID || '';
--- src/registerGuildCommands.ts
+++ src/registerGuildCommands.ts
@@ -1,14 +1,15 @@
 import {REST} from 'discord.js';
 import {Commands} from './commands/Commands';
-import {APP_ID, GUILD_ID, TOKEN} from './config';
+import {APP_ID, TOKEN} from './config';
 import {Routes} from 'discord-api-types/rest/v10';
 
 const registerGuildCommands = async () => {
     try {
+        const guildId = '1234';
         // TODO Take GuildId from onGuildCreate
         const rest = new REST({version: '10'}).setToken(TOKEN);
         console.log('Refresh Guild Commands....');
-        await rest.put(Routes.applicationGuildCommands(APP_ID, GUILD_ID), {
+        await rest.put(Routes.applicationGuildCommands(APP_ID, guildId), {
             body: Commands
         });
         console.log('Successfully reloaded Guild Commands');
@@ -16,5 +17,3 @@ const registerGuildCommands = async () => {
         console.warn(error);
     }
 };
-
-registerGuildCommands();
