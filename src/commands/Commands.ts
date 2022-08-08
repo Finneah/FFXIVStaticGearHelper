f12a5600af2d1b3a33679022b085b7b9cce0e43d
Finneah <jennifer.schnaible86@gmail.com>
2022-08-08 23:42:13 +0200

    feat: remove TestCommand

--- src/commands/Commands.ts
+++ src/commands/Commands.ts
@@ -1,6 +1,5 @@
 import {Command} from './Command';
 
 import {ShowEtroBisById} from './slashCommands/ShowEtroBisById/ShowEtroBisById';
-import {Test} from './slashCommands/Test/Test';
 
-export const Commands: Command[] = [ShowEtroBisById, Test];
+export const Commands: Command[] = [ShowEtroBisById];
